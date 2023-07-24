4423960e3055e20ce4f503278f65a2a5a16d5282
이찬 <cksdlakstp12@naver.com>
2023-07-24 19:36:15 +0900

    update

---
+++ train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name pedestrian
+#SBATCH --error=error/train_error.txt
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=4
+#SBATCH --mem-per-gpu=5G
+#SBATCH --time=24:00:00
+#SBATCH --partition batch_ce_ugrad
+#SBATCH -o slurm/logs/slurm-%A-%x.out
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=cksdlakstp12@khu.ac.kr
+
+pip install easydict
+pip install Pillow==8.3.0
+
+cd src
+
+CUDA_VISIBLE_DEVICES=0 python train_eval.py
+
+exit 0
