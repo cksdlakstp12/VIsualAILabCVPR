55ac5e3a61d9ae4552ab3f05267c6673f70ef1c0
이찬 <cksdlakstp12@naver.com>
2023-08-30 02:47:09 +0900

    update

--- src/inference.py
+++ src/inference.py
@@ -48,7 +48,7 @@ def val_epoch(model: SSD300, dataloader: DataLoader, dataset_type: str, input_si
     results = dict()
     with torch.no_grad():
         for i, blob in enumerate(tqdm(dataloader, desc='Evaluating')):
-            if dataset_type is "KAISTPedWS":
+            if dataset_type == "KAISTPedWS":
                 image_vis, image_lwir, vis_boxes, lwir_boxes, vis_labels, lwir_labels, indices, _ = blob
             else:
                 image_vis, image_lwir, boxes, labels, indices = blob
