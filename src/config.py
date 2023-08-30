87fc10d5b32a3a9fbcf9509ea7c6cc9c599830b8
lee_chan <57341543+cksdlakstp12@users.noreply.github.com>
2023-08-30 14:04:04 +0900

    Update config.py

--- src/config.py
+++ src/config.py
@@ -24,6 +24,7 @@ train = edict()
 
 train.day = "all"
 train.img_set = f"Labeled_Unlabled_combine.txt"
+train.teacher_img_set = "./imageSets/Unlabeled_90.txt"
 
 train.checkpoint = None ## Load chekpoint
 
@@ -155,7 +156,6 @@ args["test"].co_transform = Compose([Resize(test.input_size), \
 
 ## for soft teacher
 args.props_path = "./props.txt"
-args.txt_path = "./imageSets/Unlabeled_90.txt"
 args.cnvt_path = "./convert.txt"
 
 args.tau = 0.0001
